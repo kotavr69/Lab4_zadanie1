72e3fd07e1fc8edd1d3669b7e5b0c31a49017e17
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-10-17 17:34:08 +0700

    Добавьте файлы проекта.

---
+++ lablab4.1/lablab4.1.cpp
@@ -0,0 +1,23 @@
+﻿/*Вариант 11
+Дан набор ненулевых целых чисел;
+признак его завершения — число 0.
+Вывести количество отрицательных нечетных чисел в наборе.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int number;
+	int k = 0;
+	while (true) {
+		cin >> number;
+		if (number == 0) {
+			break;
+		}
+		if (((number % 2) != 0) && (number < 0)){
+			k++;
+		}
+	}
+	cout << "Количество отрицательных нечетных чисел: " << k << endl;
+}
